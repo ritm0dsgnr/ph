c251654339baa7fbbc7275952d5243878038596b
Daria <ritm0dsgnr@gmail.com>
2023-03-19 18:32:48 +0500

    Правки

--- js/script.js
+++ js/script.js
@@ -1,12 +1,62 @@
-var intro__title = document.querySelector('.intro__title');
-var hand__1 = document.querySelector('.hand__1');
+$(function() {
+    
+$("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
 
-hand__1.onmouseover = function(e) {
-  intro__title.style.transform = 'translate(-125px)';
-  hand__1.style.right = '0';
-};
+    var $this = $(this),
+        blockId = $this.data('scroll'),
+        blockOffset = $(blockId).offset().top;
 
-hand__1.onmouseout = function(e) {
-  intro__title.style.transform = 'translate(0)';
-  hand__1.style.right = '-125px';
+    $("html, body").animate({
+        scrollTop:  blockOffset
+    }, 600);
+});
+
+$(".burger").on("click", function(event) {
+    $(".burger").toggleClass("active");
+    $(".burger__nav").toggleClass("active");
+    $("body").toggleClass("lock");
+});
+
+$(".nav__link").on("click", function(event) {
+    $(".burger__nav").removeClass("active");
+    $(".burger").removeClass("active");
+    $("body").removeClass("lock");
+});
+
+$(".intro__container div").hover(function(){
+    $(".hand__1").css("right", "-70px");
+    $(".intro__title").css("right", "90px");
+    }, function(){
+    $(".hand__1").css("right", "-200px");
+    $(".intro__title").css("right", "0px");
+});
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('anim-show');
+        }
+        else {
+            change.target.classList.remove('anim-show');
+        }
+    });
+}
+let options = {
+    threshold: [0.5] 
 };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.anim');
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
+});
+
+let obj = $(".anim");
+let scrollChange = 1;
+$(window).scroll(function() {
+    let scroll = $(window).scrollTop();
+    if (scroll >= scrollChange) {
+        obj.addClass('anim-show');
+    }
+});
